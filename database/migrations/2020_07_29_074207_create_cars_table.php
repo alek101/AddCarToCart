5afd37f9b72bf37f2d9e7027c12b7196203d4f18
alek101 <47416711+alek101@users.noreply.github.com>
2020-07-29 10:06:42 +0200

    version 0.1
    migration

---
+++ database/migrations/2020_07_29_074207_create_cars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->char('naziv',50);
+            $table->char('slika',100);
+            $table->bigInteger('brand');
+            $table->integer('cena');
+            $table->smallInteger('godiste');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
